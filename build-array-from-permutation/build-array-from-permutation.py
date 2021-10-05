544adc860dac64f6cf8f23a63a372c77f5de4f05
Muhammad Sufyan Jilani <86870164+SufyanJilani67@users.noreply.github.com>
2021-10-05 12:17:42 +0500

    Time: 128 ms (38.74%), Space: 14.6 MB (8.25%) - LeetHub

--- build-array-from-permutation/build-array-from-permutation.py
+++ build-array-from-permutation/build-array-from-permutation.py
@@ -1,10 +1,13 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-         # Method 1
-        output = []
-        for i in nums:
-            output.append(nums[i])
-        return output
-    
-        # Method 2 (List Comprehension)
-       # return [nums[i] for i in nums]
+            ans = []
+            for i in range(len(nums)):
+                value = nums[nums[i]]
+                ans.append(value)
+                
+            return ans
+                                 
+            array = []
+            for i in nums:
+                array.append(nums[i])
+            return array
