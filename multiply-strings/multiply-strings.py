dc5ddbbb6d6c47a5354acb15b48287dbaa70d6e2
Muhammad Sufyan Jilani <86870164+SufyanJilani67@users.noreply.github.com>
2021-10-15 17:25:51 +0500

    Time: 32 ms (88.58%), Space: 14.3 MB (56.29%) - LeetHub

--- multiply-strings/multiply-strings.py
+++ multiply-strings/multiply-strings.py
@@ -1,11 +1,6 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        
-        def conversion(num):
-            dic = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
-            ans = 0
-            for i in num:
-                ans = ans*10 + dic[i]
-            return ans
-        
-        return str(conversion(num1)*conversion(num2))
+        num1 = int(num1)
+        num2 = int(num2)
+        m = num1*num2
+        return str(m)
